c4c361d55c345ae661fb787ce0be0aab67d8927e
DESKTOP-03EL7EH\davin <davinwjiaya20@gmail.com>
2022-01-18 15:36:49 +0700

    - Add frontend home page

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,11 +1,16 @@
-<?php namespace App\Controllers;
+<?php
+
+namespace App\Controllers;
+
+use App\Models\home_model;
 
 class Home extends BaseController
 {
 	public function index()
 	{
-		return view('welcome_message');
+		return view('index');
 	}
+
 
 	//--------------------------------------------------------------------
 
---
+++ app/Views/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Direct.in</title>
+    <link rel="stylesheet" href="/CSS/style.css">
+    <link rel="shortcut icon" href="/assets/logo-header.png">
+</head>
+
+<body style="margin:0px;">
+
+    <header class="header-home">
+        <img class="logo-header" src="/assets/logo-header.png">
+    </header>
+
+    <section class="content-home">
+        <div class="content-detail">
+            <div class="content-heading">
+                <h1 class="heading-info">URL Shortener</h1>
+            </div>
+
+            <div class="input-link">
+                <input type="text" class="long-link" placeholder="Paste your long url">
+            </div>
+
+            <div class="link-alias">
+                <span class="fix-link">http://direct.in/</span>
+                <input type="text" class="link-alias-insert" placeholder="alias">
+                <font class="text-alias">Put your alias here</font>
+            </div>
+
+            <div class="btn-create">
+                <button class="btn-create-detail">Create</button>
+            </div>
+
+            <div class="side-icon">
+                <img class="side-icon-detail" src="/assets/side-icon.png">
+            </div>
+        </div>
+    </section>
+
+    <footer class="footer-home">
+        <div>
+            <div class="footer-detail">
+                <p class="copyright-text">&copy; Codeness 2022</p>
+            </div>
+            <div class="btn-donation" style="display:inline-block">
+                <button class="btn-donation-detail"><b>DONATION</b></button>
+            </div>
+        </div>
+    </footer>
+
+</body>
+
+</html>
